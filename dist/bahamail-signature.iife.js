c0982e236b67a458462f7d689c4a621a99e93885
maple3142 <kirby741852963@gmail.com>
2018-03-17 17:39:11 +0800

    build

--- dist/bahamail-signature.iife.js
+++ dist/bahamail-signature.iife.js
@@ -1,9 +1,9 @@
 
 // ==UserScript==
-// @name         巴哈姆特郵件簽名檔
+// @name         巴哈姆特站內信簽名檔
 // @namespace    https://blog.maple3142.net/
-// @version      0.1
-// @description  try to take over the world!
+// @version      0.2
+// @description  幫巴哈姆特站內信的站內信加上簽名檔功能
 // @author       maple3142
 // @match        https://mailbox.gamer.com.tw/send.php*
 // @match        https://mailbox.gamer.com.tw/reply.php*
